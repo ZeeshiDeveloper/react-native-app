1e1aabc174d9f716588e0ad857ce3dce02820ee6
Zeeshan Ali <zee03044163645@gmail.com>
2023-02-28 21:15:46 +0500

    Working on Login Page.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+// for navbar
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./SCreens/HomeScreen";
+import LoginScreen from "./SCreens/LoginScreen";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	console.log("Debug by using console log");
 	// let c;
 	// c.toString();
 	return (
-		<View style={styles.container}>
-			<Text>Hello React Native</Text>
-			<StatusBar style="auto" />
-		</View>
+		// <View style={styles.container}>
+		// 	<Text>Hello React Native</Text>
+		// 	<StatusBar style="auto" />
+		// </View>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Login"
+					options={{ headerShown: false }}
+					component={LoginScreen}
+				/>
+				{/* <Stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} /> */}
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 }
 
